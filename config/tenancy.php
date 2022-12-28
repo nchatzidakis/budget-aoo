c2ef1416d6c3e7bc29b613ff2494721ba231957c
Nikolas Chatzidakis <nchatzidakis@users.noreply.github.com>
2022-12-28 23:17:27 +0200

    various fixes

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,8 +17,8 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        env('APP_URL'),
         'localhost',
-        env('APP_URL')
     ],
 
     /**
